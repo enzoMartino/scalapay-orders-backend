c7c28fded289d4cc8f9df37ed6bd9248827c0024
vmartino <enzo.martino.94@gmail.com>
2024-02-01 21:07:27 +0100

    feat: add RedisModule

---
+++ src/common/redis/services/redis.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ClientCartDto } from '../dtos/client-cart.dto';
+
+@Injectable()
+export class RedisService {
+  private readonly logger = new Logger(RedisService.name);
+
+  async getClientCart(clientId: string): Promise<ClientCartDto> {
+    this.logger.debug(`[getClientCart] clientId: ${clientId}`);
+
+    if (clientId !== '123') {
+      return null;
+    }
+
+    return {
+      items: [
+        {
+          name: 'T-Shirt',
+          category: 'clothes',
+          sku: '12341234',
+          quantity: 1,
+          price: {
+            amount: '10.00',
+            currency: 'EUR',
+          },
+        },
+      ],
+      totalAmount: {
+        amount: '10.00',
+        currency: 'EUR',
+      },
+    };
+  }
+}
