88aa82c57adb8d9819d1095e2e2168bca77ea576
vmartino <enzo.martino.94@gmail.com>
2024-02-01 23:59:14 +0100

    refactor: remove useless tests

--- src/orders/v1/controllers/orders-v1.controller.spec.ts
+++
@@ -1,5 +0,0 @@
-import { OrdersV1Controller } from './orders-v1.controller';
-
-describe('OrdersV1Controller', () => {
-  let controller: OrdersV1Controller;
-});
--- src/orders/v1/services/orders-v1.service.spec.ts
+++
@@ -1,5 +0,0 @@
-import { OrdersV1Service } from './orders-v1.service';
-
-describe('OrdersV1Service', () => {
-  let service: OrdersV1Service;
-});
