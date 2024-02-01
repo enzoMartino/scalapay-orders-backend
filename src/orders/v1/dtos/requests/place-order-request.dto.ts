3cdda458418668400ff2fa3abc8e4eecfd3134f0
vmartino <enzo.martino.94@gmail.com>
2024-02-01 21:11:26 +0100

    feat: add PlaceOrderV1RequestDto

---
+++ src/orders/v1/dtos/requests/place-order-request.dto.ts
@@ -0,0 +1,11 @@
+import { Expose, Type } from 'class-transformer';
+import { IsNotEmpty, ValidateNested } from 'class-validator';
+import { ShippingDetailsDto } from 'src/common/dtos/shipping-details.dto';
+
+export class PlaceOrderV1RequestDto {
+  @Expose()
+  @IsNotEmpty({ always: true })
+  @ValidateNested({ always: true })
+  @Type(() => ShippingDetailsDto)
+  shippingDetails: ShippingDetailsDto;
+}
