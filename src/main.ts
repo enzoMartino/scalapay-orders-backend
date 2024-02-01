b6d20ee88a691218fefcda84365614d5ea531a53
vmartino <enzo.martino.94@gmail.com>
2024-02-02 00:36:39 +0100

    chore: enable cors only for local environment

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
+
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
-  app.enableCors({ origin: 'http://localhost:4200', credentials: true });
+
+  if (process.env.ENV === 'local') {
+    logger.debug(`env: ${process.env.ENV}`);
+
+    app.enableCors({ origin: 'http://localhost:4200', credentials: true });
+  }
 
   await app.listen(3000);
 }
