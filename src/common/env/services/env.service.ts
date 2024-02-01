b0cc07ad374c67c754ae765bca7151a62d7755ac
vmartino <enzo.martino.94@gmail.com>
2024-02-01 21:04:49 +0100

    feat: add EnvModule

---
+++ src/common/env/services/env.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Merchant } from 'src/scalapay/dtos/requests/create-order.request';
+
+@Injectable()
+export class EnvService {
+  private readonly logger = new Logger(EnvService.name);
+
+  constructor(private readonly configService: ConfigService) {}
+
+  getMerchantUrls(): Merchant {
+    const redirectConfirmUrl = this.configService.get(
+      'MERCHANT_REDIRECT_CONFIRM_URL',
+    );
+
+    const redirectCancelUrl = this.configService.get(
+      'MERCHANT_REDIRECT_CANCEL_URL',
+    );
+
+    return {
+      redirectConfirmUrl,
+      redirectCancelUrl,
+    };
+  }
+
+  getScalapayApisHostname(): string {
+    return this.configService.get('SCALAPAY_APIS_HOSTNAME');
+  }
+
+  getScalapayOrdersApisEndpoint(): string {
+    return this.configService.get('SCALAPAY_ORDERS_APIS_ENDPOINT');
+  }
+
+  getScalapayApisVersion(): string {
+    return this.configService.get('SCALAPAY_APIS_VERSION');
+  }
+
+  getScalapayApisBearerToken(): string {
+    return this.configService.get('SCALAPAY_APIS_BEARER_TOKEN');
+  }
+
+  getScalapayOrdersUrl(): string {
+    const scalapayApisHostname = this.getScalapayApisHostname();
+    const scalapayApisVersion = this.getScalapayApisVersion();
+    const scalapayOrdersApisEndpoint = this.getScalapayOrdersApisEndpoint();
+
+    return `${scalapayApisHostname}/${scalapayApisVersion}/${scalapayOrdersApisEndpoint}`;
+  }
+}
